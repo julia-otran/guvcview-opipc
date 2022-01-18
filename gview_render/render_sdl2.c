90a1acdac5043754eeb6667045939bee8d6a988a
Júlia Otranto <julia.otranto@outlook.com>
2022-01-17 21:08:27 -0300

    YUV to RGB neon

--- gview_render/render_sdl2.c
+++ gview_render/render_sdl2.c
@@ -29,7 +29,8 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <turbojpeg.h>
-// #include <armneon.h>
+#include <arm_neon.h>
+#include <stdio.h>
 
 #include "gview.h"
 #include "gviewrender.h"
@@ -53,6 +54,177 @@ int frame_height;
 int run = 1;
 
 int sdl_init_flags;
+
+#define LOAD_Y(i,j) (pY + i * width + j)
+#define LOAD_V(i,j) (pV + i * (width / 2) + (j / 2))
+#define LOAD_U(i,j) (pU + i * (width / 2) + (j / 2))
+
+const uint8_t ZEROS[8] = {220,220, 220, 220, 220, 220, 220, 220};
+const uint8_t Y_SUBS[8] = {16, 16, 16, 16, 16, 16, 16, 16};
+const uint8_t UV_SUBS[8] = {128, 128, 128, 128, 128, 128, 128, 128};
+
+const uint8_t COLOR_MAX[8] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+const uint8_t COLOR_MIN[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+const uint8_t ALPHAS[8] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+
+void color_convert_common(unsigned char *pY, unsigned char *pU, unsigned char *pV, int width, int height, unsigned char *buffer, int grey)
+{
+
+  int i, j;
+  unsigned char *out = buffer;
+
+  int16x8_t Y_SUBvec = (int16x8_t) vmovl_u8(vld1_u8(Y_SUBS));
+  int16x8_t UV_SUBvec = (int16x8_t) vmovl_u8(vld1_u8(UV_SUBS)); // v,u,v,u v,u,v,u
+  
+  int16x8_t COLOR_MAXvec = (int16x8_t) vmovl_u8(vld1_u8(COLOR_MAX));
+  int16x8_t COLOR_MINvec = (int16x8_t) vmovl_u8(vld1_u8(COLOR_MIN));
+
+  uint8x8_t ZEROSvec =vld1_u8(ZEROS);
+  uint8x8_t ALPHAvec = vld1_u8(ALPHAS);
+ 
+  int16x8_t nUvec;
+  int16x8_t nVvec;
+
+    // YUV 4:2:2
+    for (i = 0; i < height; i++)
+    {
+      for (j = 0; j < width; j += 8)
+      {
+        //        nY = *(pY + i * width + j);
+        //        nV = *(pUV + (i / 2) * width + bytes_per_pixel * (j / 2));
+        //        nU = *(pUV + (i / 2) * width + bytes_per_pixel * (j / 2) + 1);
+
+        uint8x8_t nYvec;
+        uint8x16_t nYLoadvec;
+	uint8x16_t nULoadvec;
+	uint8x16_t nVLoadvec;
+	uint8x8_t nTempUvec;
+	uint8x8_t nTempVvec;
+
+        int16x4_t nUvec16_4;
+        int16x4_t nVvec16_4;
+
+	if (j % 16 == 0) {
+	    nYLoadvec = vld1q_u8(LOAD_Y(i,j));
+	    nYvec = vget_low_u8(nYLoadvec);
+
+	    if (j % 32 == 0) {
+	        nULoadvec = vld1q_u8(LOAD_U(i,j));
+		nVLoadvec = vld1q_u8(LOAD_V(i,j));
+		nTempUvec = vget_low_u8(nULoadvec);
+		nTempVvec = vget_low_u8(nVLoadvec);
+	    } else {
+		nTempUvec = vget_high_u8(nULoadvec);
+		nTempVvec = vget_high_u8(nVLoadvec);
+	    }
+
+	    nUvec = (int16x8_t) vmovl_u8(nTempUvec);
+	    nUvec = vsubq_s16(nUvec, UV_SUBvec);
+	    nUvec16_4 = vget_low_s16(nUvec);
+
+	    nVvec = (int16x8_t) vmovl_u8(nTempVvec);
+	    nVvec = vsubq_s16(nVvec, UV_SUBvec);
+            nVvec16_4 = vget_low_s16(nVvec);	    
+        } else {
+	    nYvec = vget_high_u8(nYLoadvec);
+            nUvec16_4 = vget_high_s16(nUvec);
+            nVvec16_4 = vget_high_s16(nVvec);
+	}
+
+	int32x4_t nUvec32_4 = vmovl_s16(nUvec16_4);
+	int32x4_t nVvec32_4 = vmovl_s16(nVvec16_4);
+
+        // uint8x8_t nUVvec = vld1_u8(LOAD_V(i,j)); // v,u,v,u v,u,v,u
+	// uint16x8_t nUVvec = vzip_u8(nUvec, nVvec);
+
+        // nYvec = vmul_u8(nYvec, vcle_u8(nYvec,ZEROSvec));
+
+        // Yuv Convert
+        //        nY -= 16;
+        //        nU -= 128;
+        //        nV -= 128;
+
+        //        nYvec = vsub_u8(nYvec, Y_SUBvec);
+        //        nUVvec = vsub_u8(nYvec, UV_SUBvec);
+
+        // int16x8_t nYvec16 = vsubq_s16((int16x8_t) vmovl_u8(nYvec), Y_SUBvec);
+	int16x8_t nYvec16 = (int16x8_t) vmovl_u8(nYvec);
+        // uint16x8_t nUVvec16 = vmovl_u8(vsub_u8(nUVvec, UV_SUBvec));
+
+        int16x4_t Y_low4 = vget_low_s16(nYvec16);
+        int16x4_t Y_high4 = vget_high_s16(nYvec16);
+        
+	// uint16x4_t UV_low4 = vget_low_u16(nUVvec16);
+	// uint16x2 U_low2 = vget_low_u16(nUvec8);
+	// uint16x2 V_low2 = vget_low_u16(nVvec8);
+
+        // uint16x4_t UV_high4 = vget_high_u16(nUVvec16);
+	// uint16x2 U_high2 = vget_high_u16(nUvec8);
+	// uint16x2 V_high2 = vget_high_u16(nVvec8);
+
+        // uint32x4_t UV_low4_int = vmovl_u16(UV_low4);
+        // uint32x4_t UV_high4_int = vmovl_u16(UV_high4);
+
+        int32x4_t Y_low4_int = vmull_n_s16(Y_low4, 1000);
+        int32x4_t Y_high4_int = vmull_n_s16(Y_high4, 1000);
+
+        // uint32x4x2_t UV_uzp = vuzpq_u32(UV_low4_int, UV_high4_int);
+
+        // uint32x2_t Vl = vget_low_u32(UV_uzp.val[0]);// vld1_u32(UVvec_int);
+        // uint32x2_t Vh = vget_high_u32(UV_uzp.val[0]);//vld1_u32(UVvec_int + 2);
+
+	int32x2_t Vl = vget_low_s32(nVvec32_4);// vld1_u32(UVvec_int);
+        int32x2_t Vh = vget_high_s32(nVvec32_4);
+	
+        int32x2x2_t Vll_ = vzip_s32(Vl, Vl);
+        int32x4_t* Vll = (uint32x4_t*)(&Vll_);
+
+        int32x2x2_t Vhh_ = vzip_s32(Vh, Vh);
+        int32x4_t* Vhh = (uint32x4_t*)(&Vhh_);
+
+        int32x2_t Ul =  vget_low_s32(nUvec32_4);
+        int32x2_t Uh =  vget_high_s32(nUvec32_4);
+
+        int32x2x2_t Ull_ = vzip_s32(Ul, Ul);
+        int32x4_t* Ull = (uint32x4_t*)(&Ull_);
+
+        int32x2x2_t Uhh_ = vzip_s32(Uh, Uh);
+        int32x4_t* Uhh = (uint32x4_t*)(&Uhh_);
+
+        int32x4_t B_int_low = vmlaq_n_s32(Y_low4_int, *Ull, 1402); //multiply by scalar accum
+        int32x4_t B_int_high = vmlaq_n_s32(Y_high4_int, *Uhh, 1402); //multiply by scalar accum
+        int32x4_t G_int_low = vsubq_s32(Y_low4_int, vmlaq_n_s32(vmulq_n_s32(*Vll, 714), *Ull, 344));
+        int32x4_t G_int_high = vsubq_s32(Y_high4_int, vmlaq_n_s32(vmulq_n_s32(*Vhh, 714), *Uhh, 344));
+        int32x4_t R_int_low = vmlaq_n_s32(Y_low4_int, *Vll, 1772); //multiply by scalar accum
+        int32x4_t R_int_high = vmlaq_n_s32(Y_high4_int, *Vhh, 1772); //multiply by scalar accum
+
+        B_int_low = vshrq_n_s32 (B_int_low, 10);
+        B_int_high = vshrq_n_s32 (B_int_high, 10);
+        G_int_low = vshrq_n_s32 (G_int_low, 10);
+        G_int_high = vshrq_n_s32 (G_int_high, 10);
+        R_int_low = vshrq_n_s32 (R_int_low, 10);
+        R_int_high = vshrq_n_s32 (R_int_high, 10);
+
+
+	int16x8_t R_combined = vcombine_s16(vqmovn_s32 (R_int_low),vqmovn_s32 (R_int_high));
+	int16x8_t G_combined = vcombine_s16(vqmovn_s32 (G_int_low),vqmovn_s32 (G_int_high));
+	int16x8_t B_combined = vcombine_s16(vqmovn_s32 (B_int_low),vqmovn_s32 (B_int_high));
+
+	R_combined = vmaxq_s16(vminq_s16(R_combined, COLOR_MAXvec), COLOR_MINvec);
+	G_combined = vmaxq_s16(vminq_s16(G_combined, COLOR_MAXvec), COLOR_MINvec);
+	B_combined = vmaxq_s16(vminq_s16(B_combined, COLOR_MAXvec), COLOR_MINvec);
+
+        uint8x8x4_t RGB;
+        RGB.val[0] = vmovn_u16((uint16x8_t) R_combined);
+        RGB.val[1] = vmovn_u16((uint16x8_t) G_combined);
+        RGB.val[2] = vmovn_u16((uint16x8_t) B_combined);
+	RGB.val[3] = ALPHAvec;
+
+        vst4_u8 (out+i*width*4 + j*4, RGB);
+      }
+    }
+}
 
 /*
  * initialize sdl video
@@ -238,9 +410,10 @@ static int video_init2(int width, int height, int flags)
 		// SDL_PIXELFORMAT_IYUV,  // yuv420p
 		// SDL_PIXELFORMAT_RGB24,
 		// SDL_PIXELFORMAT_ARGB32,
-		//SDL_PIXELFORMAT_RGBX8888,
-		SDL_PIXELFORMAT_RGBA32,
+		// SDL_PIXELFORMAT_RGBX8888,
+		SDL_PIXELFORMAT_RGBA32, // Seems to be the more faster
 		// SDL_PIXELFORMAT_YUY2,
+		// SDL_PIXELFORMAT_BGRA32,
 		SDL_TEXTUREACCESS_STREAMING,
 		width,
 		height);
@@ -294,12 +467,18 @@ void* render_loop() {
 		
 		if (srcPlanes[0] && srcPlanes[1] && srcPlanes[2]) {
 			tjDecodeYUVPlanes(tj, srcPlanes, NULL, TJSAMP_422, tmp_frame, width, width * 4, height, TJPF_RGBX, 0);
+			// color_convert_common(srcPlanes[0], srcPlanes[1], srcPlanes[2], width, height, tmp_frame, 0);
 		} else {
 			// printf("Else ???\n");
 		}
 
     		// use for RGBA
-                SDL_UpdateTexture(rending_texture, NULL, tmp_frame, frame_width * 4);
+		// SDL_UpdateTexture(rending_texture, NULL, tmp_frame, frame_width * 4);
+		SDL_UpdateTexture(rending_texture, NULL, tmp_frame, frame_width * 4);
+
+		// why this is so slow??
+                // SDL_UpdateTexture(rending_texture, NULL, srcPlanes[0], frame_width);
+		
 		// SDL_UpdateYUVTexture(rending_texture, NULL, yplane, frame_width, uplane, frame_width / 2, vplane, frame_width / 2); 
                 // SDL_UpdateTexture(rending_texture, NULL, tmp_frame, frame_width * 2);
 		SDL_RenderCopy(main_renderer, rending_texture, NULL, NULL);
