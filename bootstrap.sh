083ca631654c2e54f1cacb48e53200f88fdc70e9
Júlia Otranto Aulicino <julia.otranto@outlook.com>
2023-02-08 12:09:36 -0300

    Move dep install to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
  
+sudo apt install -y intltool pkg-config libjson-c-dev libv4l-dev libudev-dev libusb-1.0-0 libusb-1.0-0-dev libdrm-dev libtool libgettextpo0 libgettextpo-dev libglib2.0-dev
+
 # To be safe include -I flag
 autoreconf --force --verbose --install
 ./configure --config-cache $*
