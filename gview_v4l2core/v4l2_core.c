080192649812a0fd45f3584a49f7163b955d39f0
Júlia Otranto Aulicino <julia.otranto@outlook.com>
2025-04-01 02:30:23 -0300

    Adjust check

--- gview_v4l2core/v4l2_core.c
+++ gview_v4l2core/v4l2_core.c
@@ -1775,7 +1775,7 @@ v4l2_dev_t* v4l2core_init_dev(const char *device)
 	vd->tilt_step = 128;
 
 	/*open device*/
-	if ((vd->fd = v4l2_open(vd->videodevice, O_RDWR | O_NONBLOCK, 0)) < 0)
+	if ((vd->fd = v4l2_open(vd->videodevice, O_RDWR | O_NONBLOCK, 0)) <= 0)
 	{
 		fprintf(stderr, "V4L2_CORE: ERROR opening V4L interface: %s\n", strerror(errno));
 		clean_v4l2_dev(vd);
